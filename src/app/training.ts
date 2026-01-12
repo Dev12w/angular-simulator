22361747ec52f47b62c4fc4b05cc7dc3c41801e3
Dev12w <absa0505@gmail.com>
2026-01-13 00:59:20 +0300

    fix

--- src/app/training.ts
+++ src/app/training.ts
@@ -1,10 +1,12 @@
-interface IUser {
+// 6. Создать интерфейс, который описывает юзера. Поля на ваш выбор. Одно поле должно быть опциональным.
+export interface IUser {
   id: number;
   name: string;
   surname: string;
   age?: number;
 }
 
+// 7. Создать интерфейс, который расширяется интерфейсом User с задания №5 и имеет свои дополнительные поля 
 interface IEmployee extends IUser {
   position: string;
   status: Status;
@@ -13,12 +15,20 @@ interface IEmployee extends IUser {
 type Status = 'loading' | 'success' | 'error';
 type TextFormat = 'uppercase' | 'lowercase' | 'capitalize';
 
+// 4. Создать переменную status, которая может быть только: "loading", "success", "error".
+let status: Status;
+
+// 5. Создать переменную textFormat, которая может быть только: 'uppercase', 'lowercase', 'capitalize'".
+let textFormat: TextFormat;
+
+// 3. Создать функцию, которая принимает 2 числа и возвращает их сумму. Полностью типизировать параметры, значение, возвращаемое функцией.
 const sum = (a: number, b: number): number => {
   return a + b;
 }
 
 sum(2, 4);
 
+// 8. Создать функцию, которая принимает строку и вариант,  как именно форматировать строку (задание №5) и на основе этого возвращает форматированную строку.
 const formatText = (text: string, format: TextFormat): string => {
   switch (format) {
     case 'uppercase':
@@ -36,12 +46,14 @@ formatText('angular simulator', 'capitalize');
 formatText('angular simulator', 'uppercase');
 formatText('Angular Simulator', 'lowercase');
 
+// 9. Создать функцию, которая принимает строку и символ, возвращает строку без переданного символа.  (есть специальные методы для этого, гуглим)
 const removeSymbol = (str: string, symbol: string): string => {
-  return str.replace(symbol, "");
+  return str.replaceAll(symbol, "");
 };
 
-console.log(removeSymbol('Text', 'e'))
+removeSymbol('Text', 'e')
 
+// 10. Создать массив объектов на основе интерфейса с задания №6. Отфильтровать его по одному из параметров
 const users: IEmployee[] = [
   {
     id: 1,
@@ -66,4 +78,4 @@ const users: IEmployee[] = [
   }
 ]
 
-const filterUsersStatus: IEmployee[] = users.filter(user => user.status === 'success')
+const filteredUsersByStatus = users.filter(user => user.status === 'success')
