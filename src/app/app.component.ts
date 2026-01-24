0eabd367472e673f6837aa93aae2aab96fc710f6
Dev12w <absa0505@gmail.com>
2026-01-24 18:51:26 +0300

    Completed all tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Color } from '.././enums/Color';
+import './collection';
 import './training';
 
 @Component({
@@ -7,6 +9,31 @@ import './training';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
+
 export class AppComponent {
+
+  lastVisitDateKey = 'lastVisitDate';
+  visitCounterKey = 'visitCounter';
+
+  constructor() {
+    this.saveLastVisitDate();
+    this.incrementVisitCounter();
+  }
+
+  companyName: string = 'Румтибет';
+
+  saveLastVisitDate() {
+    const date = new Date();
+    localStorage.setItem(this.lastVisitDateKey, date.toISOString());
+  }
+
+  incrementVisitCounter() {
+    const visitCounter = Number(localStorage.getItem(this.visitCounterKey)) || 0;
+    localStorage.setItem(this.visitCounterKey, `${visitCounter + 1}`);
+  }
+
+  isMainColor(color: Color): boolean {
+    return (color === Color.BLUE || color === Color.GREEN || color === Color.RED);
+  }
 
 }
---
+++ src/app/collection.ts
@@ -0,0 +1,64 @@
+export class Collection<T> {
+
+  constructor(private items: T[]) { }
+
+  getItems(): T[] {
+    return this.items;
+  }
+
+  getItem(index: number): T | undefined {
+    return this.items.at(index);
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+
+  remove(index: number): void {
+    const filtered = this.items.filter((v, i) => i != index);
+    this.items = filtered;
+  }
+
+  replace(index: number, newItem: T): void {
+    if (this.items.at(index)) {
+      this.items[index] = newItem;
+    }
+  }
+
+}
+
+interface Product {
+  id: number;
+  name: string;
+}
+
+const productCollection = new Collection<Product>([
+  {
+    id: 1,
+    name: 'Apple'
+  },
+  {
+    id: 2,
+    name: 'Samsung'
+  },
+  {
+    id: 3,
+    name: 'Honor'
+  },
+  {
+    id: 4,
+    name: 'Xiaomi'
+  },
+]);
+
+productCollection.getItem(0);
+productCollection.replace(2, { id: 5, name: 'Huawey' });
+productCollection.remove(1);
+productCollection.clear();
+
+const col1 = new Collection(['Apple', 'Samsung', 'Honor', 'Xiaomi']);
+
+col1.getItem(0);
+col1.replace(2, 'Honor Replaced');
+col1.remove(1);
+col1.clear();
