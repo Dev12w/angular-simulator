e7d40cc4a62fcf71927c98eb2bcdbd2eb0ea064b
Dev12w <absa0505@gmail.com>
2026-03-02 14:12:14 +0300

    stash

---
+++ src/home-page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import { Message } from '../message/message.component'
+
+@Component({
+  selector: 'app-home-page',
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+})
+export class HomePageComponent {
+
+
+}
