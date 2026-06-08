3ef658c1e520d3449d67cd653c42e772face2955
Dev12w <absa0505@gmail.com>
2026-06-08 11:13:07 +0300

    fix

--- src/app/interceptor/http-logging.interceptor.ts
+++ src/app/interceptor/http-logging.interceptor.ts
@@ -4,7 +4,7 @@ import { catchError, tap, throwError } from 'rxjs';
 export const httpLoggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
   const startTime: number = Date.now();
 
-  const logMessages = (status: number, isError: boolean = false ): void => {
+  const logMessage = (status: number, isError: boolean = false ): void => {
     const message: string = `${ req.method } ${ req.url } ${ status } ${ Date.now() - startTime } мс`;
     isError ? console.error(`Ошибка запроса: ${ message }`) : console.log(message);
   };
@@ -12,11 +12,11 @@ export const httpLoggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unkno
   return next(req).pipe(
     tap((event: HttpEvent<unknown>) => {
       if (event instanceof HttpResponse) {
-        logMessages(event.status);
+        logMessage(event.status);
       }
     }),
     catchError((error: HttpErrorResponse) => {
-      logMessages(error.status, true);
+      logMessage(error.status, true);
       return throwError(() => error);
     })
   );
