b4361ccb2cef820cb7f00ca0d9db982dfafc0e8b
Dev12w <absa0505@gmail.com>
2026-06-21 00:58:34 +0300

    Completed all tasks (23.2)

--- src/app/interceptor/http-logging.interceptor.ts
+++ src/app/interceptor/http-logging.interceptor.ts
@@ -4,16 +4,22 @@ import { catchError, tap, throwError } from 'rxjs';
 export const httpLoggingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
   const startTime: number = Date.now();
 
+  const logMessage = (status: number, isError: boolean = false ): void => {
+    const message: string = `${ req.method } ${ req.url } ${ status } ${ Date.now() - startTime } мс`;
+    isError ? console.error(`Ошибка запроса: ${ message }`) : console.log(message);
+  };
+
   return next(req).pipe(
     tap((event: HttpEvent<unknown>) => {
       if (event instanceof HttpResponse) {
-        console.log(`${ req.method } ${ req.url } ${ event.status } ${ Date.now() - startTime } мс`);
+        logMessage(event.status);
       }
     }),
-
     catchError((error: HttpErrorResponse) => {
-      console.error(`Ошибка запроса: ${ req.method } ${ req.url } ${ error.status } ${ Date.now() - startTime } мс`);
+      logMessage(error.status, true);
       return throwError(() => error);
     })
   );
 };
+
+
